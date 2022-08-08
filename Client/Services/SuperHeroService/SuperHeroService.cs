46f15dbf38fcd9c988e77ff75cec441f32fac911
Richard Chalk <barmybrit@gmail.com>
2022-08-08 14:12:42 +0200

    Get superheroes working

--- Client/Services/SuperHeroService/SuperHeroService.cs
+++ Client/Services/SuperHeroService/SuperHeroService.cs
@@ -1,6 +1,35 @@
-﻿namespace SuperHeroes.Client.Services.SuperHeroService
+﻿using System.Net.Http.Json;
+
+namespace SuperHeroes.Client.Services.SuperHeroService
 {
-    public interface SuperHeroService : ISuperHeroService
+    public class SuperHeroService : ISuperHeroService
     {
+        private readonly HttpClient _http;
+
+        public SuperHeroService(HttpClient http)
+        {
+            _http = http;
+        }
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
+
+        public Task GetComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetSingleSuperHero(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var result = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+            if (result != null)
+            {
+                Heroes = result;
+            }
+        }
     }
 }
