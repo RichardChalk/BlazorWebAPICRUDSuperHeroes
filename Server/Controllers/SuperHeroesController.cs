a8b144c1e02f3df28c20708378e369447b75d943
Richard Chalk <barmybrit@gmail.com>
2022-08-08 08:06:11 +0200

    Added get single hero in controller

--- Server/Controllers/SuperHeroesController.cs
+++ Server/Controllers/SuperHeroesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SuperHeroes.Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] // eg. https://localhost:7067/api/SuperHeroes
     [ApiController]
     public class SuperHeroesController : ControllerBase
     {
@@ -36,14 +36,27 @@ namespace SuperHeroes.Server.Controllers
         //  READ -  READ -  READ -  READ -  READ -  READ -  READ -  READ -  READ
         //  READ -  READ -  READ -  READ -  READ -  READ -  READ -  READ -  READ
         //  READ -  READ -  READ -  READ -  READ -  READ -  READ -  READ -  READ
+        
         [HttpGet]
-
         // Returning an ActionResult means we can also return a status code 
         // as well as our heroes
         public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
         {
             return Ok(Heroes);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleSuperHero(int id)
+        {
+            var hero = Heroes.FirstOrDefault(h => h.Id == id);
+            if (hero == null)
+            {
+                return BadRequest("No hero found with that Id!");
+            }
+            
+            return Ok(hero);
+        }
+
 
     }
 }
