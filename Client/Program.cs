acf1f3a6633e5d4c88cfd162903a140181e47878
Richard Chalk <barmybrit@gmail.com>
2022-08-08 09:37:19 +0200

    Started services injection

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SuperHeroes.Client;
+using SuperHeroes.Client.Services.SuperHeroService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Inject my services
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,6 @@
+﻿namespace SuperHeroes.Client.Services.SuperHeroService
+{
+    public interface SuperHeroService : ISuperHeroService
+    {
+    }
+}
